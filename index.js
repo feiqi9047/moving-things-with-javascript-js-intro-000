c893108308bb362142a0f088156a44350db91b0a
Fei Qi <feiqi9047@gmail.com>
2018-11-20 16:07:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ document.addEventListener('keydown', function(e){
   }
 })
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
